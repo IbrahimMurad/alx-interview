781a445b3a97c28e8161955780b4cd2ecdfef01e
IbrahimMurad <134874559+IbrahimMurad@users.noreply.github.com>
2024-06-11 02:05:05 +0000

    reducing the code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-""" This module defines a function that calculates the fewest number of operations needed to result in exactly n H characters in the file
+""" This module defines a function that calculates
+the fewest number of operations needed
+to result in exactly n H characters in the file
 It also defines all the required function to solve the problem.
 """
 
 
-def primes(n: int) -> list[int]:
-    """ returns a list of all the prime numbers up to n 
-    except for 2 """
-    return [i for i in range(3, n + 1) if all([i % j != 0 for j in range(2, i)])]
-
-
-def minOperations(n: int) -> int:
+def minOperations(n: int, primes: list[int] = []) -> int:
     """ returns the minimum number of operations
     needed to result in exactly n H characters in the file """
 
     if n < 2:
         return 0
-    if n == 2:
-        return 2
-    if n % 2 == 0:
-        return 2 + minOperations(int(n / 2))
-    for i in primes(n):
-        if n == i:
-            return i
-        if n % i == 0:
-            return i + minOperations(int(n / i))
+    if not primes:
+        primes = [
+            i for i in range(2, n + 1)
+            if all([i % j != 0 for j in range(2, i)])
+            ]
+    for prime in primes:
+        if n % prime == 0:
+            return prime + minOperations(int(n / prime), primes)
     return 0
