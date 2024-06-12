539b6150f3cafba0c006295243409a76148c1a3c
IbrahimMurad <134874559+IbrahimMurad@users.noreply.github.com>
2024-06-12 04:06:12 +0000

    recreate primes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,18 +6,17 @@ It also defines all the required function to solve the problem.
 """
 
 
-def minOperations(n: int, primes: list[int] = []) -> int:
+def minOperations(n: int) -> int:
     """ returns the minimum number of operations
     needed to result in exactly n H characters in the file """
 
+    primes = [
+        i for i in range(2, n + 1)
+        if all([i % j != 0 for j in range(2, i)])
+    ]
     if n < 2:
         return 0
-    if not primes:
-        primes = [
-            i for i in range(2, n + 1)
-            if all([i % j != 0 for j in range(2, i)])
-            ]
     for prime in primes:
         if n % prime == 0:
-            return prime + minOperations(int(n / prime), primes)
+            return prime + minOperations(int(n / prime))
     return 0
