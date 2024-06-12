3cefaa2fc4f95f128ae8420367458d87aa663fed
IbrahimMurad <134874559+IbrahimMurad@users.noreply.github.com>
2024-06-12 04:50:18 +0000

    a small change

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,19 +4,21 @@ the fewest number of operations needed
 to result in exactly n H characters in the file
 It also defines all the required function to solve the problem.
 """
+from typing import List, Optional
 
 
-def minOperations(n: int) -> int:
+def minOperations(n: int, primes: Optional[List[int]] = None) -> int:
     """ returns the minimum number of operations
     needed to result in exactly n H characters in the file """
 
-    primes = [
-        i for i in range(2, n + 1)
-        if all([i % j != 0 for j in range(2, i)])
-    ]
     if n < 2:
         return 0
+    if not primes:
+        primes = [
+            i for i in range(2, n + 1)
+            if all([i % j != 0 for j in range(2, i)])
+        ]
     for prime in primes:
         if n % prime == 0:
-            return prime + minOperations(int(n / prime))
+            return prime + minOperations(n // prime)
     return 0
