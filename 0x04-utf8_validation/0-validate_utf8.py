0aca7e73da702a50f5af55b774c8dd7180e32a44
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-26 10:39:08 +0300

    valid first byte but invalid 2nd and 3rd bytes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,11 +8,11 @@ from typing import List
 def num_of_bytes(n: int) -> int:
     """ returns true if n is a valid byte """
     if n >> 7:
-        if ~(n >> 3) == 0b11110:
+        if (n >> 3) == 0b11110:
             return 4
-        elif ~(n >> 4) == 0b1110:
+        elif (n >> 4) == 0b1110:
             return 3
-        elif ~(n >> 5) == 0b110:
+        elif (n >> 5) == 0b110:
             return 2
         else:
             return 0
@@ -25,7 +25,7 @@ def validUTF8(data: List[int]) -> bool:
     data_length = len(data)
     while i < data_length:
         points = num_of_bytes(data[i])
-        if points == 0:
+        if points == 0 or (i + points) > data_length:
             return False
         for j in range(points - 1):
             if data[i + j + 1] >> 6 != 0b10:
