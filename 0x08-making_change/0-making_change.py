6ca0e8d6d0ab6b95347ee6dc9547014eb77de61c
IbrahimMurad <ibrahimmorad31@gmail.com>
2024-07-22 13:04:22 +0300

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Making Change module
+"""
+
+
+def makeChange(coins: list[int], total: int):
+    """ calculates the fewest number of coins needed
+    to meet a given amount total using the coins provided in coins list
+
+    Args:
+        coins (list[int]): a list of the values of the coins in your possession
+        total (_type_): the given amount
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        count += total // coin
+        total %= coin
+    return count if total == 0 else -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,3 @@
+# Making Change
+
+This project is a simple example of a dynamic programming problem. The goal is to make change for a given amount of money using the fewest coins possible. The program will output the number of coins of each denomination that make up the total amount of change.
